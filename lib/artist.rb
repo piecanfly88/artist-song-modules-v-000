5c18fae0b371b30f6fd35eeec33a5ef976620e32
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-06-10 07:38:06 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 class Artist
+  extend Memorable
   attr_accessor :name
   attr_reader :songs
 
@@ -18,14 +19,6 @@ class Artist
   def self.all
     @@artists
   end
-  # 
-  # def self.reset_all
-  #   self.all.clear
-  # end
-  #
-  # def self.count
-  #   @@artists.count
-  # end
 
   def add_song(song)
     @songs << song
