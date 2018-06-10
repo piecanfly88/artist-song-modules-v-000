b5c12eb523edf96bbf59230ead5cb8744eacd70e
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-06-10 08:27:46 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,11 @@
 module Memorable
+  modele ClassMethods
+    def reset_all
+      self.all.clear
+    end
 
-  def reset_all
-    self.all.clear
-  end
-
-  def count
-    self.all.count
-  end
-  
+    def count
+      self.all.count
+    end
+  end  
 end
