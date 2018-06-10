d8dd744e1e560f8fedfdb9bb481575aa8a20e2fe
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-06-10 08:02:30 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,14 +2,15 @@ require 'pry'
 
 class Artist
   extend Memorable
+  extend Findable
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def initialize
     @@artists << self
