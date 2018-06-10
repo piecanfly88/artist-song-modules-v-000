1df004f8df0b42160e0caaed04c2628b9031f9d7
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-06-10 06:51:16 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,14 +18,14 @@ class Artist
   def self.all
     @@artists
   end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    @@artists.count
-  end
+  # 
+  # def self.reset_all
+  #   self.all.clear
+  # end
+  # 
+  # def self.count
+  #   @@artists.count
+  # end
 
   def add_song(song)
     @songs << song
